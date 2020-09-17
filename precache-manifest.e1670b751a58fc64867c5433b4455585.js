49d83700dd915dbfa2c63921ccad3f41d80f8071
Diego Pineda <pineda.diego798@gmail.com>
2020-09-16 22:04:10 -0500

    Updates

---
+++ precache-manifest.e1670b751a58fc64867c5433b4455585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076a3c1e52a408cdcb4f9b6550a11468",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a0cd60e7d1ea038aaafc",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30aa2f0ea61258e3c697",
+    "url": "/portfolio/static/js/2.f96d775e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f96d775e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cd60e7d1ea038aaafc",
+    "url": "/portfolio/static/js/main.ac09f75c.chunk.js"
+  },
+  {
+    "revision": "8b2185634292915c87ea",
+    "url": "/portfolio/static/js/runtime-main.f0eff8b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
