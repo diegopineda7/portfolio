be7a25b8d93853db2e3d430235351bb8fcda560d
Diego Pineda <pineda.diego798@gmail.com>
2020-09-19 14:41:39 -0500

    Restoring Contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import emailLogo from './assets/social/email.png';
+import React from 'react';
+import email from './assets/social/email.png';
 import instagram from './assets/social/instagram.png';
 import linkedin from './assets/social/linkedin.png';
 import whatsapp from './assets/social/whatsapp.png';
@@ -7,9 +7,15 @@ import whatsapp from './assets/social/whatsapp.png';
 export default function Contact() {
   return (
     <div className='contact' id='contact'>
-      <h2>CONTACTA CONMIGO</h2>
-      <h3 className='message'>Me puedes contactar en redes sociales o enviándome un email</h3>
+      <h2>CONTACTO</h2>
+      <h3 className='message'>Me puedes contactar en mis redes sociales</h3>
       <div className='contactContent'>
+        <a href='mailto:pineda.diego798@gmail.com'>
+          <div className='social'>
+            <img src={email} alt='Email' />
+            <h3>Email</h3>
+          </div>
+        </a>
         <a
           rel="noopener noreferrer"
           target='_blank'
@@ -19,22 +25,28 @@ export default function Contact() {
             <img src={linkedin} alt='LinkedIn' />
             <h3>LinkedIn</h3>
           </div>
-          <div className='formItem'>
-            <label>¿A qué email te puedo responder?</label>
-            <input name='email' type='email' placeholder='Email*' ref={emailRef} onChange={changeInput} />
-          </div>
-          <div className='formItem'>
-            <label>¿Qué quieres comentarme?</label>
-            <textarea name='message' type='text' placeholder='Mensaje*' ref={messageRef} onChange={changeInput} />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target='_blank'
+          href='https://www.instagram.com/pinedadiego7/'
+        >
+          <div className='social'>
+            <img src={instagram} alt='Instagram' />
+            <h3>Instagram</h3>
           </div>
         </a>
-        <a href='mailto:pineda.diego798@gmail.com'>
+        <a
+          rel="noopener noreferrer"
+          target='_blank'
+          href='https://api.whatsapp.com/send?phone=+573125285909'
+        >
           <div className='social'>
-            <img src={email} alt='Email' />
-            <h3>Email</h3>
+            <img src={whatsapp} alt='WhatsApp' />
+            <h3>WhatsApp</h3>
           </div>
         </a>
       </div>
-    </div >
+    </div>
   )
 }
