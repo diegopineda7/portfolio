82aa3efaf241c9c48a1d546f7eb92cebf26f882b
Diego Pineda <pineda.diego798@gmail.com>
2020-09-20 22:44:38 -0500

    Education and Experience info

--- src/Education.js
+++ src/Education.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import uis from './assets/education/uis.png';
 
 export default function Education() {
   return (
     <div className='education' id='education'>
       <h2>EDUCACIÓN</h2>
+      <div className='item'>
+        <img src={uis} alt='Universidad Industrial de Santander' />
+        <div className='description'>
+          <h3>Universidad Industrial de Santander</h3>
+          <h3>Ingeniería de Sistemas</h3>
+          <h4>Abril 2015 - Actualmente (Décimo semestre)</h4>
+          <h4>Bucaramanga, Santander, Colombia</h4>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ipred from './assets/education/ipred.png';
 
 export default function Experience() {
   return (
     <div className='experience' id='experience'>
       <h2>EXPERIENCIA LABORAL</h2>
+      <div className='item'>
+        <img src={ipred} alt='Instituto de Proyección Regional y Educación a Distancia' />
+        <div className='description'>
+          <h3>Instituto de Proyección Regional y Educación a Distancia</h3>
+          <h3>Universidad Industrial de Santander</h3>
+          <h3>Desarrollador web</h3>
+          <h4>Septiembre 2018 - Actualmente</h4>
+          <h4>Bucaramanga, Santander, Colombia</h4>
+        </div>
+      </div>
     </div>
   )
 }
